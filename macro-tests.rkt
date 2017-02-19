79133b76d5d6bf743774fb1d2cf4e486f03ba3fc
Milo Turner <iitalics@gmail.com>
2017-02-19 01:03:01 +0000

    tests from trying to understand bound-identifier=?

--- macro-tests.rkt
+++ macro-tests.rkt
@@ -18,9 +18,39 @@
        (with-syntax ([v (unbox c-box)])
          #'v))]))
 
-
 (with-counter
   (printf "~a\n" (counter!))
   (for ([_ (in-range 4)])
     (printf "-> ~a\n" (counter!)))
   (printf "~a\n" (counter!)))
+(printf "\n\n\n\n\n")
+
+
+
+(require (for-syntax racket))
+
+(define-syntax-parameter var #f)
+
+(define-syntax (with-var stx)
+  (syntax-case stx ()
+    [(_ x body ...)
+     #'(syntax-parameterize ([var #'x])
+         body ...)]))
+
+(define-syntax (relation stx)
+  (syntax-case stx ()
+    [(_ x)
+     (with-syntax ([y (syntax-parameter-value #'var)])
+       (with-syntax ([o (bound-identifier=? #'x #'y)])
+         #'o))]))
+
+(with-var a
+  (displayln (relation a)))
+
+(define-syntax (bound=? stx)
+  (syntax-case stx ()
+    [(_ x y)
+     (with-syntax ([o (bound-identifier=? #'x #'y)])
+       #'o)]))
+
+(bound=? a a)
