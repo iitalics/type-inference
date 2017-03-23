f324348a058d60aaeb7022216cbcac2420975ffe
Milo Turner <iitalics@gmail.com>
2017-03-23 19:01:01 +0000

    got extremely basic typechecking working

--- local-hints/lang.rkt
+++ local-hints/lang.rkt
@@ -1,23 +1,27 @@
 #lang turnstile
 (require (only-in racket/format ~a)
          (only-in racket/string string-join)
-         racket/match)
+         (only-in racket/function identity const)
+         racket/match
+         syntax/parse
+         (only-in racket/sequence
+                  in-syntax))
 
-(provide (type-out Int Str → all))
+(provide (type-out Int Str → ∀))
 
 (define-base-types Int Str)
 
 (define-type-constructor →
   #:arity >= 1)
 
-(define-binding-type all
+(define-binding-type ∀
   #:bvs >= 1
   #:arity = 1)
 
 (define-base-type ??)
 
 
-
+;; mod-beg: toplevel parser
 (provide (rename-out [mod-beg #%module-begin]))
 (define-syntax mod-beg
   (syntax-parser
@@ -26,6 +30,18 @@
         ; TODO: stuff here
         (void))]))
 
+;; top-repl: REPL handler
+(provide (rename-out [top-repl #%top-interaction]))
+(define-syntax top-repl
+  (syntax-parser
+    [(_ . e)
+     #:with (_ _ (e-) (τ)) (infer (list #'e))
+     #:with τ/s (type->str #'τ)
+     #'(printf "~s\n - ~a\n"
+               e-
+               'τ/s)]))
+
+;; convert type to string representation
 (define (type->str t)
   (let ([orig (match (syntax-property t 'orig)
                 [(list _ ... o) o]
@@ -36,15 +52,26 @@
                                   #:after-last ")")]
       [x (~a x)])))
 
-(provide (rename-out [top-repl #%top-interaction]))
-(define-syntax top-repl
-  (syntax-parser
-    [(_ . e)
-     #:with (_ _ (e-) (τ)) (infer (list #'e))
-     #:with τ/s (type->str #'τ)
-     #'(printf "~s\n - ~a\n"
-               e-
-               'τ/s)]))
+(begin-for-syntax
+  ;; syntax class for checking that argument counts match
+  (define-syntax-class arg#-check
+    (pattern
+     ([e ...] [τ ...])
+     #:fail-unless (stx-length=? #'(e ...) #'(τ ...))
+     (format "wrong number of arguments to function; expected ~a, got ~a"
+             (stx-length #'(τ ...))
+             (stx-length #'(e ...)))))
+
+  ;; unification algorithm
+  ;;; (unify-all (listof identifier?)
+  ;;;            syntax?)
+  ;;;   -> (listof type-stx?)
+  (define (unify-all Xs cs)
+    (with-syntax ([(X ...) Xs] [({τ1 τ2} ...) cs])
+      (for/list ([X (in-syntax #'(X ...))])
+        #'??)))
+  )
+
 
 (provide (rename-out [datum #%datum]))
 (define-typed-syntax datum
@@ -61,24 +88,45 @@
 
 (provide (rename-out [app #%app]))
 (define-typed-syntax app
+  ; normal application
   [(_ f e_i ...) ≫
-   [⊢ f ≫ f- ⇒ τ_f]
-   #:with (~→ τ_i ... τ_r) #'τ_f
-   #:fail-unless (stx-length=? #'(τ_i ...) #'(e_i ...))
-   (format "incorrect # of arguments, expected ~a"
-           (stx-length #'(τ_i ...)))
+   [⊢ f ≫ f- ⇒ (~→ τ_i ... τ_r)]
+   #:with :arg#-check #'([e_i ...] [τ_i ...])
    [⊢ e_i ≫ e_i- ⇐ τ_i] ...
    --------
-   [⊢ (#%app- f- e_i- ...) ⇒ τ_r]])
+   [⊢ (#%app- f- e_i- ...) ⇒ τ_r]]
+
+  ; ∀ inference
+  [(_ f e_i ...) ≫
+   [⊢ f ≫ f- ⇒ (~∀ (X ...) (~→ τ_i ... τ_r))]
+   #:with :arg#-check #'([e_i ...] [τ_i ...])
+   [⊢ e_i ≫ e_i- ⇒ τ_j] ...
+   #:with (σ ...) (unify-all #'(X ...)
+                             #'({τ_i τ_j} ...))
+   #:with τ_r- (substs #'(σ ...)
+                       #'(X ...)
+                       #'τ_r)
+   --------
+   [⊢ (#%app- f- e_i- ...) ⇒ τ_r-]]
+
+  ; ??
+  [(_ f e_i ...) ≫
+   [⊢ f ≫ _ ⇒ τ_f]
+   --------
+   [#:error (type-error #:src #'f
+                        #:msg "cannot apply type: ~a" #'τ_f)]]
+  )
 
 (provide (rename-out [lam lambda]))
 (define-typed-syntax lam #:datum-literals (:)
   [(_ ([x_i:id : τ_i:type] ...) e) ≫
-   [[x_i ≫ x_i- ⇒ τ_i.norm] ... ⊢ e ≫ e- ⇒ τ_r]
+   [[x_i ≫ x_i- : τ_i.norm] ... ⊢ e ≫ e- ⇒ τ_r]
    --------
    [⊢ (lambda- (x_i- ...) e-) ⇒ (→ τ_i.norm ... τ_r)]])
 
 
-(provide (rename-out [t+ +]))
-(define-primop t+
-  + : (→ Int Int Int))
+(provide
+ (typed-out [+ : (→ Int Int Int)]
+            [[identity : (∀ (X) (→ X X))] id]
+            [const : (∀ (X) (→ X (∀ (Y) (→ Y X))))]
+            ))
