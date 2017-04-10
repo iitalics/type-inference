57714ff2b01657ccd8dd20b2aa1de9c1752b84b0
Milo Turner <iitalics@gmail.com>
2017-04-10 16:44:26 +0000

    usage file displays features better

--- local-hints/usage.rkt
+++ local-hints/usage.rkt
@@ -1,14 +1,35 @@
 #lang s-exp "lang.rkt"
 
+; datum
 1                          ; -> 1 : Int
-2                          ; -> 2 : Int
+"hi"                       ; -> "hi" : Str
+
+; simple application
 (add1 3)                   ; -> 4 : Int
 (+ 5 3)                    ; -> 8 : Int
-(nat-rec {Int} 4 add1 5)   ; -> 9 : Int
+
+; lambdas
 (lambda ([x : Int]) "hi")       ; #<proc> : (→ Int Str)
 (lambda ([x : Int]) x)          ; #<proc> : (→ Int Int)
 (ann (lambda (x) x) : (→ Str Str)) ; #<proc> : (→ Str Str)
 
+; polymorphic instantiation
+(nat-rec {Int}
+         1
+         (lambda (x) (+ x x))
+         5)                ; -> 32 : Int
+
+; polymorphic inference (bidirectional)
 (nat-rec 1
          (lambda ([x : Int]) (+ x x))
          5)                ; -> 32 : Int
+
+; polymorphic inference (partial; bidirectional)
+(with-rand 10 (lambda (x) "hi")) ; -> "hi" : Str
+(with-rand 10 (lambda (x) (+ x x))) ; -> ... : Int
+
+; TODO:
+; polymorphic inference (backtracking; bidirectional)
+#;(nat-rec 1
+           (lambda (x) (+ x x))
+           5)                ; -> 32 : Int
