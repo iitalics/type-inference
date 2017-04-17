c61728d3dd5ae752a8391ff05c6e43aec677f18d
Milo Turner <iitalics@gmail.com>
2017-04-17 13:24:32 +0000

    changes

--- bt/lang.rkt
+++ bt/lang.rkt
@@ -2,7 +2,8 @@
 (require (for-syntax racket
                      racket/syntax
                      syntax/parse)
-         (for-template syntax/parse))
+         (for-meta -1 syntax/parse)
+         (for-meta 2 syntax/parse))
 
 
 ;;;; LOW LEVEL TYPE / SYNTAX UTILITIES ;;;;
@@ -23,84 +24,61 @@
       [else
        (let* ([t* (attach t 'ctx 'type-eval)]
               [τ (local-expand t* 'expression '())])
-         (unless (prop τ '#%type)
+         #;(unless (prop τ '#%type)
            (raise-syntax-error #f "not a type" src))
          τ)]))
 
   )
 
-(define-syntax define-type
+(define-syntax define-base-type
   (syntax-parser
-
-    ;;; basic type ;;;
-    [(_ x:id)
-     #:with x/pat (format-id #'x "~~~a" (syntax-e #'x))
-     #:with x/int (generate-temporary #'x)
+    [(_ B:id)
+     #:with B/pat (format-id #'B "~~~a" (syntax-e #'B))
+     #:with B/int (format-id #'B "internal:~a" (syntax-e #'B))
      #'(begin
-         ; internal representation
-         (define (x/int . _) (error "evaluating type at runtime"))
-
-         ; external transformer
-         (define-syntax x
-           (make-set!-transformer
-            (syntax-parser
-              [_:id
-               #:fail-unless (eq? (prop this-syntax 'ctx) 'type-eval) "type used in wrong context"
-               (attach #'(#%app x/int) '#%type #t)]
-              [(a . b)
-               #:fail-when #t "type takes no arguments"
-               #f])))
-
-         ; pattern expander
          (begin-for-syntax
-           (define-syntax x/pat
+           (define-syntax B/pat
              (pattern-expander
-              (lambda (s) (syntax-case s ()
-                       [_ #'((~literal #%plain-app)
-                             (~literal x/int))]))))))]
+              (syntax-parser
+                [_ #'((~literal #%plain-app)
+                      (~literal B/int))]))))
 
-    ;;; constructor ;;;
-    [(_ x:id #:arity ar-pred)
-     #:with x/pat (format-id #'x "~~~a" (syntax-e #'x))
-     #:with x/int (generate-temporary #'x)
+         (define (B/int)
+           (error "use of type" 'B "as expression"))
+         (define-syntax (B -stx-)
+           (syntax-case -stx- ()
+             [(_ . _) (raise-syntax-error #f "type doesn't take arguments" -stx-)]
+             [_ (attach #'(#%app B/int) '#%type #t)]))
+         )]))
+
+(define-syntax define-ctor-type
+  (syntax-parser
+    [(_ B:id . _)
+     #:with B/pat (format-id #'B "~~~a" (syntax-e #'B))
+     #:with B/int (format-id #f "internal:~a" (syntax-e #'B))
      #'(begin
-         ; internal representation
-         (define (x/int . _) (error "evaluating type at runtime"))
-
-         ; external transformer
-         (define-syntax x
-           (let ([ar? ar-pred])
-             (syntax-parser
-               [(_ ts (... ...))
-                #:fail-unless (eq? (prop this-syntax 'ctx) 'type-eval) "type used in wrong context"
-                #:fail-unless (ar? (length (syntax-e #'(ts (... ...)))))
-                (format "wrong number of arguments to type ~a" 'x)
-                #:with (τs (... ...)) (map type-eval (syntax-e #'(ts (... ...))))
-                (attach #'(x/int (lambda () τs (... ...))) '#%type #t)])))
-
-         ; pattern expander
          (begin-for-syntax
-           (define-syntax x/pat
+           (define-syntax B/pat
              (pattern-expander
-              (lambda (s) (syntax-case s ()
-                       [(_ . ps) #'((~literal #%plain-app)
-                                    (~literal x/int)
-                                    . _)]))))))]
-    ))
+              (syntax-parser
+                [(_ . p) #'((~literal #%plain-app)
+                            (~literal B/int)
+                            (#%plain-lambda () . p))]))))
+
+         (define (B/int)
+           (error "use of type" 'B "as expression"))
+         (define-syntax (B -stx-)
+           (syntax-case -stx- ()
+             [(_ . args)
+              (attach #'(#%app B/int (lambda () . args))
+                      '#%type #t)]))
+         )]))
 
 
-;;;; PROTOTYPES ;;;;
 
-(define-type ??)
-
-
-
-;;;; INFERING FROM MACROS ;;;;
+;;;; TYPE INFERENCE MECHANICS ;;;;
 
 (begin-for-syntax
-  ;; infer the type of an expression
-  ;; returns a list of the output expression, the type, and the bound variables
-  ;; optionally provide a prototype or context
   (define (infer e
                  #:ctx [ctx-alist '()]
                  #:proto [π (type-eval #'??)])
@@ -126,7 +104,9 @@
 
   ;; this is used to seperate expressions in introduced context
   ;; from resulting expressions
-  (define --ctx-barrier-- (lambda (i) i))
+  (define --ctx-barrier-- (lambda (i)
+                            (printf "== leftover context barrier! ==\n")
+                            i))
 
   ;; wrap expression in a context
   ;; returns a new expression
@@ -134,7 +114,7 @@
     (foldl (lambda (pr e)
              (match-let ([(cons x τ) pr])
                #`(lambda (#,x)
-                   (let-syntax ([x (make-typed-alias (syntax #,x) (quote #,τ))])
+                   (let-syntax ([#,x (make-typed-alias (syntax #,x) (quote #,τ))])
                      #,e))))
            #`(#%plain-app --ctx-barrier-- #,e)
            ctx))
@@ -148,8 +128,7 @@
         [((~literal #%plain-lambda) (x:id) e-)  (trav #'e- (cons #'x xs))]
         [((~literal let-values) () e-)          (trav #'e- xs)]
         [((~literal #%expression) e-)           (trav #'e- xs)]
-        [((~literal #%plain-app)
-          (~literal --ctx-barrier--) e-)
+        [((~literal #%plain-app) (~literal --ctx-barrier--) e-)
          (values #'e- xs)]
         [u (raise-syntax-error #f "unexpected syntax in trim" #'u)])))
 
@@ -158,86 +137,29 @@
   (define (make-typed-alias e τ)
     (make-set!-transformer
      (syntax-parser
-       [_:id (attach e 'inferred-type τ)]
-       [(a . b)
-        #'(app a . b)])))
+       [_:id (inferred e τ)]
+       [(a . b) #'(app a . b)])))
   )
 
-(define-syntax define-typed-alias
+
+
+
+
+
+;;;; ACTUAL LANGUAGE ;;;;
+
+(provide (rename-out [mod #%module-begin]))
+(define-syntax mod
   (syntax-parser
-    [(_ [e x-new] (~datum :) t)
-     (let ([τ (type-eval #'t)])
-       #`(define-syntax x-new
-           (make-typed-alias #'e (quote #,τ))))]))
+    [(_ . _) #'(#%module-begin (printf "hello.\n"))]))
 
 
+(define-base-type Int)
+(define-ctor-type ->)
 
-;;;; THE ACTUAL LANGUAGE ;;;;
-
-(define-type Int)
-(define-type Str)
-(define-type → #:arity (>=/c 1))
-
-(provide Int Str →)
-
-(define msg "hello")
-(define-typed-alias [msg msg/t] : Str)
-(provide (rename-out [msg/t msg]))
-
-(define-typed-alias [+ plus] : (→ Int Int Int))
-(provide (rename-out [plus +]))
-
-
-(provide (rename-out [module-begin #%module-begin]))
-(define-syntax module-begin
-  (syntax-parser
-    [_ #'(#%module-begin (displayln "--- Hello --"))]))
-
-(provide (rename-out [top-eval #%top-interaction]))
-(define-syntax top-eval
+(provide (rename-out [top #%top-interaction]))
+(define-syntax top
   (syntax-parser
     [(_ . e)
-     #:with (e- τ _) (infer #'e)
-     #'(printf "~s : ~a\n"
-               e- 'τ)]))
-
-(provide (rename-out [dat #%datum]))
-(define-syntax dat
-  (syntax-parser
-    [(_ . k:integer) (inferred #'(#%datum . k) #'Int)]
-    [(_ . k:str)     (inferred #'(#%datum . k) #'Str)]
-    [(_ . k) (raise-syntax-error #f "unsupported datum" #'k)]))
-
-(provide :int)
-(define-syntax :int
-  (syntax-parser
-    [(_ e)
-     #:with (e- τ _) (infer #'e #:proto (type-eval #'Int))
-     (syntax-parse #'τ
-       [~Int #'e-]
-       [_ (raise-syntax-error #f "not type int!" this-syntax)])]))
-
-(provide :exp-type)
-(define-syntax :exp-type
-  (syntax-parser
-    [(_ t)
-     #:with τ (type-eval (type-eval #'t))
-     #:do [(syntax-parse #'τ
-             [(~→ x ...) (printf "its a function.\n")]
-             [_ (void)])]
-     (inferred #''τ
-               #'??)]))
-
-(provide (rename-out [app #%app]))
-(define-syntax app
-  (syntax-parser
-    [(_ fun arg ...)
-     #:with (fun- fun_τ _) (infer #'fun)
-     #:do [(syntax-parse #'fun_τ
-             [((~literal #%plain-app) _
-               ((~literal #%plain-lambda) τs ...))
-              (printf "; its a constructor of some kind.\n")]
-             [(a . b)
-              (printf "; first part of pair:\n~a\n" #'a)]
-             [_ (printf "; it's nothing.")])]
-     (inferred #''ok #'??)]))
+     #:with (~-> t ...) (type-eval #'(-> Int Int))
+     #'(displayln '(t ...))]))
